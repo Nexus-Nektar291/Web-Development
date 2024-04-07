ddc7ded27afe8313fa71669bb27128dc458834cf
Muhammad Jilani <120803991+Nexus-Nektar291@users.noreply.github.com>
2024-04-07 20:17:33 +0500

    Add files via upload

---
+++ Digital Clock/script.js
@@ -0,0 +1,11 @@
+const hrs = document.getElementById("hrs");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+setInterval(() => 
+{
+    const time = new Date();
+    hrs.innerHTML = time.getHours();
+    min.innerHTML = time.getMinutes();
+    sec.innerHTML = time.getSeconds();
+}, 1000);
